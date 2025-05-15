291a56ad3ff10ffd9470589660491593a4f631a1
Md. Ibrahim Kholil <kholilbrahm4@gmail.com>
2025-05-16 00:42:38 +0400

    some midifiy

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {  BrowserRouter, HashRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
+
 
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
   
   // <BrowserRouter>
-    <HashRouter> 
+  <HashRouter>
     <App />
-   </HashRouter> 
+  </HashRouter>
+   
   // </BrowserRouter>
   //  </StrictMode>
 )
