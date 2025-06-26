11e2300e8258a32e988cd99cf75d701babc96ea4
Md. Ibrahim Kholil <kholilbrahm4@gmail.com>
2025-06-27 02:07:34 +0400

    config file update

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-//   base: "/portfolio",
-//   build: {
-//         rollupOptions: {
-//             output:{
-//                 manualChunks(id) {
-//                     if (id.includes('node_modules')) {
-//                         return id.toString().split('node_modules/')[1].split('/')[0].toString();
-//                     }
-//                 }
-//             }
-//         }
-//     }
+  base: "/portfolio",
+  build: {
+        rollupOptions: {
+            output:{
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
+    }
 })
